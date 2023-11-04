10808a91d042a1cff37456bbf41128939111553a
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-04 11:40:50 -0300

    feat: improve Input , add props icon

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,30 +1,45 @@
+import React from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
-import FormError from '../FormError/FormError';
+import Icon from '../Icon/Icon';
 
 type IInputProps = {
-  children?: React.ReactNode
-  type?: string
-  register?: UseFormRegisterReturn
+  children?: React.ReactNode;
+  type?: string;
+  register?: UseFormRegisterReturn;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  icon?: boolean;
+  [key: string]: any;
 };
 
-function Input({ type, register }: IInputProps) {
+function Input({ type, register, onChange, className, icon, ...rest }: IInputProps) {
   return (
+    <div className="relative">
+      {icon && (
+        <div className="absolute rounded-l-lg left-0 top-0 h-full w-10 flex items-center justify-center bg-black">
+          <Icon name='SearchIcon' color='#fff' />
+        </div>
+      )}
 
-    <input
-      {...register}
-      type={type}
-      className="
-        h-10
-        bg-zinc-800
-        rounded-lg
-        p-2
-        focus:border-2
-        outline-none
-        appearance-none
-        "
-    />
-
-
+      <input
+        {...register}
+        type={type}
+        className={`
+          h-10
+          bg-zinc-800
+          rounded-lg
+          p-2
+          ${icon ? 'pl-10 ml-2' : ''}
+          focus:border-2
+          outline-none
+          appearance-none
+          text-b
+          ${className || ''}
+        `}
+        onChange={onChange}
+        {...rest}
+      />
+    </div>
   );
 }
 
