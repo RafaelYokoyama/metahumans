47ed5d4eba4769d547bc64e4f4e0ba0e84d66475
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-04 16:02:00 -0300

    feat: improve screen login

--- src/components/organisms/LoginContainer/LoginContainer.tsx
+++ src/components/organisms/LoginContainer/LoginContainer.tsx
@@ -1,10 +1,10 @@
-"use client"
-import React from 'react';
-import { useAuth } from '@/contexts/AuthContext';
-import { useRouter } from 'next/navigation';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import ChatInputField from '@/components/molecules/ChatInputField/ChatInputField';
-import Button from '@/components/atoms/Button/Button';
+"use client";
+import React, { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import ChatInputField from "@/components/molecules/ChatInputField/ChatInputField";
+import Button from "@/components/atoms/Button/Button";
 
 type IInputs = {
   login: string;
@@ -12,30 +12,54 @@ type IInputs = {
 };
 
 function LoginContainer() {
-  const { register, handleSubmit, formState: { errors } } = useForm<IInputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IInputs>();
   const { setUser } = useAuth();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit: SubmitHandler<IInputs> = (data: IInputs) => {
-    setUser({ username: data.login, password: data.password });
-    router.push('/dashboard');
+  const onSubmit: SubmitHandler<IInputs> = async (data: IInputs) => {
+    try {
+      setIsLoading(true);
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setUser({ username: data.login, password: data.password });
+      router.push("/dashboard");
+    } catch (error) {
+      console.error("Erro no login:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleGuestLogin = () => {
+    router.push("/dashboard");
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col  p-4 gap-6 text-gray-400">
-      <ChatInputField
-        register={register('login', { required: true })}
-        label='Nome de Usuario'
-        errorMessage={errors.login && 'Usuário é obrigatório' || ''}
-      />
-      <ChatInputField
-        register={register('password', { required: true })}
-        label='Senha'
-        errorMessage={errors.password && 'Senha é obrigatório' || ''}
-      />
-      <Button type="submit">Entrar</Button>
-    </form>
+    <div className="flex flex-col  p-4 text-gray-400">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col  gap-5 ">
+        <ChatInputField
+          register={register("login", { required: true })}
+          label="Nome de Usuario"
+          errorMessage={(errors.login && "Usuário é obrigatório") || ""}
+        />
+        <ChatInputField
+          register={register("password", { required: true })}
+          label="Senha"
+          errorMessage={(errors.password && "Senha é obrigatório") || ""}
+        />
+        <Button type="submit">{isLoading ? "Entrando..." : "Entrar"}</Button>
+      </form>
+
+      <Button onClick={handleGuestLogin} className="bg-slate-700">
+        Entrar como convidado
+      </Button>
+    </div>
   );
-};
+}
 
 export default LoginContainer;
