d6a6e2b863d3cc65f0d4b5d13aec547474a6a81f
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-04 15:07:43 -0300

    feat: improve Header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import Input from '@/components/atoms/Input/Input';
+import { AuthContext } from '@/contexts/AuthContext';
+import CustomAvatar from '@/components/atoms/CustomAvatar/CustomAvatar';
+import Title from '@/components/atoms/Title/Title';
 
 type IHeaderProps = {
   searchTerm: string;
@@ -8,29 +11,28 @@ type IHeaderProps = {
 };
 
 function Header({ searchTerm, handleSearchChange }: IHeaderProps) {
+  const { user } = useContext(AuthContext);
+
+  const renderLoggedInHeader = () => (
+    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+      <div className='flex items-center gap-3'>
+        <CustomAvatar size={30} />
+        <Title>{user?.username || 'Sem Nome'}</Title>
+      </div>
+    </Typography>
+  );
+
+  const renderLoggedOutHeader = () => (
+    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+      Hero 🦸
+    </Typography>
+  );
+
   return (
     <Box sx={{ flexGrow: 1, marginBottom: 7 }}>
-      <AppBar
-        position="fixed"
-        sx={{
-          background: 'rgb(10 20 80 / 74%)',
-          backdropFilter: 'blur(20px)',
-        }}
-      >
+      <AppBar position="fixed" sx={{ background: 'rgb(10 20 80 / 74%)', backdropFilter: 'blur(20px)' }}>
         <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{
-              flexGrow: 1,
-              display: { xs: 'none', sm: 'block' },
-              fontWeight: 600,
-              fontStyle: 'italic',
-            }}
-          >
-            Hero 🦸
-          </Typography>
+          {user ? renderLoggedInHeader() : renderLoggedOutHeader()}
           <Input
             type="text"
             value={searchTerm}
