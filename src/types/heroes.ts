3c9712dc0e6fbc9b7606a3accc74ab3c779768ae
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-04 10:28:54 -0300

    feat: create type Heroes

---
+++ src/types/heroes.ts
@@ -0,0 +1,18 @@
+export type Heroes = {
+  id: number
+  name: string
+  powerstats: {
+    intelligence: number
+    strength: number
+    speed: number
+    durability: number
+    power: number
+    combat: number
+  }
+  images: {
+    xs: string
+    sm: string
+    md: string
+    lg: string
+  }
+};
