396e5a6051816bf1b6b064997c48eb0f1d300532
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-04 15:58:10 -0300

    feat: improve button

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,31 +1,32 @@
 import Icon, { IconName } from "../Icon/Icon";
+import React, { ButtonHTMLAttributes } from "react";
 
-type IButtonProps = {
-  type?: "button" | "submit" | "reset" | undefined;
+type IButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   icon?: IconName;
   iconSize?: number;
+  className?: string;
 };
 
-function Button({ children, type, icon, iconSize }: IButtonProps) {
+function Button({ children, icon, iconSize, className, ...rest }: IButtonProps) {
   return (
     <button
-      type={type}
-      className="
-      min-w-28
-      text-white
-      font-bold
-      h-10
-      rounded-xl
-      hover:opacity-80
-      active:opacity-100
-      my-2
-      bg-[#3a3caf]
-      flex
-      items-center
-      justify-center
-      gap-2
-      "
+      className={`
+        min-w-28
+        text-white
+        font-bold
+        h-10
+        rounded-xl
+        hover:opacity-80
+        active:opacity-100
+        my-2
+        bg-[#3a3caf]
+        flex
+        items-center
+        justify-center
+        gap-2
+        ${className || ''}`}
+      {...rest}
     >
       {icon && (
         <span>
