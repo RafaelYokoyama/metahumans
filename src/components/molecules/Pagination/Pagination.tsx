46343b40f26d76ed62d03ccdb15e04f925f0559b
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-04 10:29:26 -0300

    feat: create component pagination

---
+++ src/components/molecules/Pagination/Pagination.tsx
@@ -0,0 +1,34 @@
+// PaginationComponent.tsx
+import React from 'react';
+import Pagination from '@mui/material/Pagination';
+import { makeStyles } from '@material-ui/styles';
+
+type PaginationProps = {
+  count: number;
+  page: number;
+  onChange: (event: React.ChangeEvent<unknown>, value: number) => void;
+};
+const useStyles = makeStyles({
+  pagination: {
+    '& .MuiPaginationItem-root': {
+      color: '#fff',
+    },
+  },
+});
+
+function PaginationComponent({ count, page, onChange }: PaginationProps) {
+  const classes = useStyles();
+
+  return (
+    <Pagination
+      count={count}
+      page={page}
+      onChange={onChange}
+      variant="outlined"
+      color="secondary"
+      classes={{ ul: classes.pagination }}
+    />
+  );
+}
+
+export default PaginationComponent;
