63baa2b81022f7d50fbdaa5618682da1c631ced7
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-05 17:49:27 -0300

    chore: configure next image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'http://homologacao3.azapfy.com.br/api/ps/metahumans',
+      'cdn.jsdelivr.net',
+      `${process.env.VERCEL_URL}`,
+    ]
+  },
+}
 
 module.exports = nextConfig
